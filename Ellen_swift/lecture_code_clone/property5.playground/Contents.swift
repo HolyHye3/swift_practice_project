556b07cb7d7bfb07b75054526cede2969a2b32f3
gimsong3 <shkim9783@gmail.com>
2023-01-20 18:40:22 +0900

    20230120 study

--- Ellen_swift/lecture_code_clone/property5.playground/Contents.swift
+++ Ellen_swift/lecture_code_clone/property5.playground/Contents.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 // 일반적인 클래스 메서드
-class Dog{
+class Dog1{
         static var species = "Dog"
         
         var name:String
@@ -22,6 +22,7 @@ class Dog{
                 sit()
         }
 
+        // 데이터를 힙에 저장하므로 자체적으로 변경가능
         func changeName(newName name: String){
                         self.name = name
                 }
@@ -30,13 +31,13 @@ class Dog{
 
 
 // 구조체의 메서드
-class Dog{
+struct Dog2{
         static var species = "Dog"
         
         var name:String
         var weight: Double
 
-        init(name: Stirng, weight: Double){
+        init(name: String, weight: Double){
                 self.name = name
                 self.weight = weight
         }
@@ -44,6 +45,10 @@ class Dog{
         func sit(){
                 print("\(name)가 앉았습니다.")
         }
+    
+    func sit(a:String){
+        print("\(a)가  앉지않았습니다")
+    }
 
         func trainning(){
                 print("왈왈 저는 \(Dog1.species)입니다.")
@@ -51,7 +56,9 @@ class Dog{
                 sit()
         }
 
-        func changeName(newName name: String){
+        // 구조체 인스턴스에서는 mutating 키워드를 붙여주어야한다.
+        // 데이터를 스택에 저장하므로 자체적으로 변경이 불가능하다.
+        mutating func changeName(newName name: String){
                         self.name = name
                 }
         
