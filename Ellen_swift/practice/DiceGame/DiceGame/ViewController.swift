1b2da20efc0fe194aeab9f7e371d6626185c97f7
gimsong3 <shkim9783@gmail.com>
2023-02-22 09:27:08 +0900

    20230222 study

---
+++ Ellen_swift/practice/DiceGame/DiceGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceGame
+//
+//  Created by 김성혜 on 2023/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
